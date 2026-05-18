443667724ddf0b1f3ab28c1ef7c943ce7f7caedb
Davide Ficocelli <davide.ficocelli.dev@gmail.com>
2026-05-18 16:20:58 +0200

    DB migration to cloud service (#8)
    - Migrated db to neon tech's cloud service - Added env.example file - Ignored .env file

--- src/config/db.ts
+++ src/config/db.ts
@@ -23,6 +23,9 @@ const pool = new Pool({
   database: process.env.DB_NAME, // database name
   password: process.env.DB_PASSWORD, // database user password
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432, // database port (usually 5432)
+  ssl: {
+    rejectUnauthorized: false, // Requested by Neon
+  },
 });
 
 // Listen for the "connect" event on the pool. This event fires when
