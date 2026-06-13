e446412ae1f9537c9b9de30a2374d593276ec7b6
Davide-Ficocelli <davide.ficocelli.dev@gmail.com>
2026-06-13 13:07:42 +0200

    Automated controller functions across modules except auth module

--- src/controllers/attributesController.ts
+++ src/controllers/attributesController.ts
@@ -5,7 +5,7 @@ import {
   updateAttributeService,
   getAllAttributesToQuestService,
 } from "../services/attributesService.ts";
-import handleResponse from "../utils/handleResponse.ts";
+import processServiceRequest from "../utils/processServiceRequest.ts";
 
 // Importing types
 import { type Response, type NextFunction } from "express";
@@ -18,32 +18,27 @@ export const createNewAttributeController = async (
   res: Response,
   next: NextFunction,
 ) => {
-  try {
-    // Gets the input fields which will be inserted in the attributes table for the new record from the request body
-    const { name, description, icon } = req.body;
+  // Gets the input fields which will be inserted in the attributes table for the new record from the request body
+  const { name, description, icon } = req.body;
 
-    // Gets user's id for attributes_id field
-    const users_id: number = req.user.id;
+  // Gets user's id for attributes_id field
+  const users_id: number = req.user.id;
 
-    // Let's create the object compliant with the NewAttribute interface
-    const newAttrDataObj: Attribute = {
-      status: "new", // TypeScript is now happy because NewAttribute requires it
-      name,
-      description,
-      icon,
-      users_id,
-    };
+  // Let's create the object compliant with the NewAttribute interface
+  const newAttrDataObj: Attribute = {
+    status: "new", // TypeScript is now happy because NewAttribute requires it
+    name,
+    description,
+    icon,
+    users_id,
+  };
 
-    // Starts the attribute creation process with the appropriate async function created in the attributesService.ts file
-    const newAttribute = await createNewAttributeService(newAttrDataObj);
-
-    // Get and return service results
-    const { ok, status, message, data } = newAttribute;
-
-    return handleResponse(res, ok, status, message, data);
-  } catch (err) {
-    next(err);
-  }
+  // Starts the attribute creation process with the appropriate async function created in the attributesService.ts file
+  return processServiceRequest(
+    res,
+    next,
+    createNewAttributeService(newAttrDataObj),
+  );
 };
 
 // Returns all of the corrispective user's attributes
@@ -52,20 +47,11 @@ export const getAttributesByUserIdController = async (
   res: Response,
   next: NextFunction,
 ) => {
-  try {
-    // Gets user id
-    const userId: number = req.user.id;
+  // Gets user id
+  const userId: number = req.user.id;
 
-    // Retrieves and saves all user's attributes
-    const userAttributes = await getAttributesByUserIdService(userId);
-
-    // Get and return service results
-    const { ok, status, message, data } = userAttributes;
-
-    return handleResponse(res, ok, status, message, data);
-  } catch (err) {
-    next(err);
-  }
+  // Retrieves and saves all user's attributes
+  return processServiceRequest(res, next, getAttributesByUserIdService(userId));
 };
 
 // Deletes an attribute
@@ -74,23 +60,15 @@ export const deleteAttributeController = async (
   res: Response,
   next: NextFunction,
 ) => {
-  try {
-    // Gets user id
-    const userId: number = req.user.id;
+  // Gets user id
+  const userId: number = req.user.id;
 
-    // Get deleted attribute
-    const deletedAttribute = await deleteAttributeService(
-      userId,
-      Number(req.params.id),
-    );
-
-    // Get and return service results
-    const { ok, status, message, data } = deletedAttribute;
-
-    return handleResponse(res, ok, status, message, data);
-  } catch (err) {
-    next(err);
-  }
+  // Get deleted attribute
+  return processServiceRequest(
+    res,
+    next,
+    deleteAttributeService(userId, Number(req.params.id)),
+  );
 };
 
 // Updates an attribute
@@ -99,25 +77,20 @@ export const updateAttributeController = async (
   res: Response,
   next: NextFunction,
 ) => {
-  try {
-    const { name, description, icon } = req.body;
-    // Gets user id
-    const userId: number = req.user.id;
+  const { name, description, icon } = req.body;
+  // Gets user id
+  const userId: number = req.user.id;
 
-    // Get updated attribute
-    const updatedAttribute = await updateAttributeService(
-      userId,
-      Number(req.params.id),
-      { name, description, icon },
-    );
-
-    // Get and return service results
-    const { ok, status, message, data } = updatedAttribute;
-
-    return handleResponse(res, ok, status, message, data);
-  } catch (err) {
-    next(err);
-  }
+  // Get updated attribute
+  return processServiceRequest(
+    res,
+    next,
+    updateAttributeService(userId, Number(req.params.id), {
+      name,
+      description,
+      icon,
+    }),
+  );
 };
 
 // Gets all attributes involved in a specific quest
@@ -126,22 +99,14 @@ export const getAllAttributesToQuestController = async (
   res: Response,
   next: NextFunction,
 ) => {
-  try {
-    // Gets user and quest id
-    const userId: number = req.user.id;
-    const questId: number = Number(req.params.questId);
+  // Gets user and quest id
+  const userId: number = req.user.id;
+  const questId: number = Number(req.params.questId);
 
-    // Get all attributes to quest
-    const allAttrsToQuest = await getAllAttributesToQuestService(
-      questId,
-      userId,
-    );
-
-    // Get and return service results
-    const { ok, status, message, data } = allAttrsToQuest;
-
-    return handleResponse(res, ok, status, message, data);
-  } catch (err) {
-    next(err);
-  }
+  // Get all attributes to quest
+  return processServiceRequest(
+    res,
+    next,
+    getAllAttributesToQuestService(questId, userId),
+  );
 };
